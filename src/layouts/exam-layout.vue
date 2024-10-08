f53dd11468b49bebe6b7c0a21121ced99fdfb997
dasturchioka <dasturchioka@gmail.com>
2024-10-08 15:22:40 +0500

    added exam layout

---
+++ src/layouts/exam-layout.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+	<div class="exam-layout">
+    <RouterView></RouterView>
+  </div>
+</template>
---
+++ src/pages/exam-page.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup></script>
+
+<template>
+	<div class="exam-page">
+    
+  </div>
+</template>
